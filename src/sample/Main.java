fc6e6e855c9ff22aabc42de9188f26392c9f0ab0
Ashok Sasitharan <asasitharan28@gmail.com>
2020-12-16 11:55:26 -0500

    Added xbox and ps5 image swap

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,18 +5,22 @@ import javafx.application.Preloader;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
@@ -28,32 +32,64 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
 
         //step 1 create a control
-        Label helloLabel = new Label("Hello, World");
-        Label goodbyeLabel = new Label("Good, Bye");
+        Label ImageLabel = new Label("XBOX Series X");
+
         Button clickMeButton = new Button("Click me");
 
+        //set up the path
+        FileInputStream imagePath = new FileInputStream("src/sample/xboxseriesx.jpg");
+        //make the image
+        Image xboxseriesx = new Image(imagePath);
+        //make the imageview
+        ImageView consoleImageView = new ImageView(xboxseriesx);
 
-        //step 1.1 configure controls
-        Font font =  Font.font("Consolas", FontWeight.BOLD,40);
-        clickMeButton.setFont(font);
-
+        consoleImageView.setFitWidth(400);
+        consoleImageView.setPreserveRatio(true);
+        FileInputStream ps5Path = new FileInputStream("src/sample/ps5.jpg");
+        Image ps5 = new Image(ps5Path);
+       // ImageView  ps5ImageView = new ImageView(ps5);
         //step 1.2 button event
-        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+      /*  clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                System.out.println("ClickMe Button clicked.");
-                helloLabel.setText("Clicked!!!");
+
+                ImageLabel.setText("PS5!!!");
+                consoleImageView.setImage(ps5);
             }
-        });
+        });*/
 
+        class ButtonClickerHandler implements EventHandler<ActionEvent>
+        {
+            @Override
+            public void handle(ActionEvent actionEvent)
+            {
+                ImageLabel.setText("PS5!!!");
+                consoleImageView.setImage(ps5);
+
+            }
+        }
+        clickMeButton.setOnAction(new ButtonClickerHandler());
         //step 2 create a container
-        GridPane gridPane = new GridPane();
-        gridPane.add(helloLabel,1,0);
-        gridPane.add(goodbyeLabel,1,1);
-        gridPane.add(clickMeButton,2,2);
-        //HBox hbox = new HBox(helloLabel,goodbyeLabel, clickMeButton);
-        //VBox vbox = new VBox(helloLabel,goodbyeLabel, clickMeButton);
+        HBox hbox = new HBox(10,ImageLabel);
 
+        HBox hbox2 =new HBox(10,consoleImageView);
+
+        hbox.setStyle("-fx-border-color: #8b06ff; -fx-alignment: center; ");
+        hbox2.setStyle("-fx-border-color: #0bf807");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.setPadding(new Insets(10));
+
+        gridPane.add(hbox,0,0);
+        gridPane.add(hbox2,0,1);
+        gridPane.add(clickMeButton,2,2);
+     //   gridPane.add(consoleImageView,3,3);
+
+       // hbox.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+        gridPane.setStyle("-fx-border-color: #8b06ff; -fx-font-size: 20px; -fx-font-family: 'Consolas'; " +
+                "-fx-font-weight: bold; -fx-cell-size: 60px");
 
 
         // step 3 add layout container to scene
